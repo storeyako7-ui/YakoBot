4a16847f12540cf6b761dba0795c358feeac962c
storeyako7-ui <storeyako7@gmail.com>
2026-04-23 17:24:55 +0100

    Add fake server setup using Express

--- index.js
+++ index.js
@@ -58,3 +58,10 @@ function start(file) {
 }
 
 start('main.js')
+
+// Puerto falso para Render
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => res.send('YakoBot Online'))
+app.listen(port, () => console.log(Servidor web falso activo en puerto ${port}))
